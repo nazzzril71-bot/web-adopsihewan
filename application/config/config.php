43a1854b794dfdb8e90f8d39cfe6ca3cd79b7659
nazriladom788-create <nazriladom788@gmail.com>
2026-07-24 20:31:04 +0700

    Fix database config and nixpacks

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://adopsihew.free.nf/?i=1';
+$config['base_url'] = 'https://web-adopsihewan-production.up.railway.app/';
 
 /*
 |--------------------------------------------------------------------------
